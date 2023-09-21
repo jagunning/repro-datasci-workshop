34c742c0720ea76c0aa6dd67c9ff173a72d65291
Jordan Gunning <youremail@arizona.edu>
2023-09-21 08:52:27 -0700

    Initial commit

---
+++ Scripts/2023-09-05 practice.R
@@ -0,0 +1,32 @@
+#practice script for the reproducibility data sci workshop
+# Jordan Gunning
+# 2023-09-96
+
+# Load packages ----
+library(readr)
+library(lme4)
+library(dplyr)
+
+# Read in data -----
+gapminder <- read.csv("Data/gapminder_data.csv")
+head(gapminder)
+str(gapminder)
+
+# Wrangle data ------------------------------------------------------------
+# command shift R to create new section
+
+gap_1980s <- filter(gapminder, year >= 1980, year < 1990)
+
+gap_big <- filter(gapminder, pop > 10000000)
+
+
+# Write data --------------------------------------------------------------
+
+write_csv(gap_1980s, "data_clean/gapminder_1980s.csv")
+
+
+
+
+
+
+
---
+++ Scripts/2023-09-05 source code.R
@@ -0,0 +1,7 @@
+# Analysis workflow
+
+# first we read and wrangle the data
+source("Scripts/2023-09-05 practice.R")
+
+
+  
