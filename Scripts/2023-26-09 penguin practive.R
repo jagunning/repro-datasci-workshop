f79b5594c3be7e07b2eec4515e6dba78f7d764cd
Jordan Gunning <jordangunning@arizona.edu>
2023-09-28 12:55:01 -0700

    function and for loop

--- Scripts/2023-26-09 penguin practive.R
+++ Scripts/2023-26-09 penguin practive.R
@@ -1,108 +1,78 @@
-# library(dplyr)
-# 
-# biscoe_data <- read.csv("https://github.com/cct-datascience/repro-data-sci/raw/r-lessons/lessons/7-intermediate-r-1/lesson-data/Biscoe.csv")
-# 
-# head(biscoe_data) 
-# 
-# anyNA(biscoe_data)
-# 
-# biscoe_data %>% na.omit()
-# 
-# biscoe_data <- biscoe_data %>% na.omit()
-# 
-# # biscoe_data_means <- biscoe_data %>% group_by(species, sex) %>% 
-# #   summarize(mean_bill_length = mean(bill_length_mm))
-# 
-# biscoe_data_means <- biscoe_data %>% group_by(species, sex) %>% 
-#   summarize(across(ends_with("mm") |  ends_with("g"), mean))
-# 
-# biscoe_data_means_imperial <- biscoe_data_means %>%
-#   mutate(across(ends_with("mm"), ~. *0.03937008, .names = "{.col}_in"),
-#          across(ends_with("g"), ~. * 0.002204623, .names = "{.col}_lb"))
-# 
-# library(stringr)
-# 
-# biscoe_data_means_imperial <- biscoe_data_means %>%
-#   mutate(across(ends_with("mm"), ~. *0.03937008, .names = "{.col}_in"))
-# 
-# biscoe_data_means_imperial <- biscoe_data_means_imperial %>% 
-#   rename(flipper_length_in = flipper_length_mm_in)
-# 
-# biscoe_data_means_imperial <- biscoe_data_means_imperial %>% 
-#   rename_with(~stringr::str_replace(., "mm_in", "in"), .cols = ends_with("mm_in")) %>% 
-#   rename_with(~stringr::str_replace(., "g_lb", "lb"), .cols = ends_with("g_lb"))
-# 
-# biscoe_data_means_imperial <- biscoe_data_means_imperial %>% 
-#   select(where(is.character) |
-#          ends_with("in") |
-#          ends_with("lb"))
-# 
-# biscoe_data_means_imperial <- biscoe_data_means_imperial %>% 
-#   select(c(where(is.character))) &
-#   ends_with("in")
-# 
-# # select pulls out columns
-# # filter pulls out rows
-  
+library(dplyr)
 
 
 # Functions ---------------------------------------------------------------
 
-my_function <- function(fav_drink = "coffee") {
+my_function <- function(island_to_use = "Biscoe") {
   
-  what_to_say <- paste0("I need ", fav_drink, "!")
+  if(island_to_use == "Biscoe") {
+    data_url <- "https://github.com/cct-datascience/repro-data-sci/raw/r-lessons/lessons/7-intermediate-r-1/lesson-data/Biscoe.csv"
+  } else if (island_to_use == "Dream"){
+    data_url <- "https://github.com/cct-datascience/repro-data-sci/raw/r-lessons/lessons/7-intermediate-r-1/lesson-data/Dream.csv"
+  } else if (island_to_use == "Torgersen"){
+    data_url <- "https://github.com/cct-datascience/repro-data-sci/raw/r-lessons/lessons/7-intermediate-r-1/lesson-data/Torgersen.csv"
+  } else {
+    stop("Island to use doesn't exist")
+
+  }
   
-  return(what_to_say)
   
-}
-
-
-
-
-
-
-my_function <- function(data_url = "https://github.com/cct-datascience/repro-data-sci/raw/r-lessons/lessons/7-intermediate-r-1/lesson-data/Biscoe.csv") {
-
   island_data <- read.csv(data_url)
   island_data <- island_data %>% na.omit()
-  
-  # island_data_means <- island_data %>% group_by(species, sex) %>% 
+
+  # island_data_means <- island_data %>% group_by(species, sex) %>%
   #   summarize(mean_bill_length = mean(bill_length_mm))
-  
-  island_data_means <- island_data %>% group_by(species, sex) %>% 
+
+  island_data_means <- island_data %>% group_by(species, sex) %>%
     summarize(across(ends_with("mm") |  ends_with("g"), mean))
-  
+
   island_data_means_imperial <- island_data_means %>%
     mutate(across(ends_with("mm"), ~. *0.03937008, .names = "{.col}_in"),
            across(ends_with("g"), ~. * 0.002204623, .names = "{.col}_lb"))
 
   # island_data_means_imperial <- biscoe_data_means %>%
   #   mutate(across(ends_with("mm"), ~. *0.03937008, .names = "{.col}_in"))
-  
-  # island_data_means_imperial <- island_data_means_imperial %>% 
+
+  # island_data_means_imperial <- island_data_means_imperial %>%
   #   rename(flipper_length_in = flipper_length_mm_in)
-  
-  island_data_means_imperial <- island_data_means_imperial %>% 
-    rename_with(~stringr::str_replace(., "mm_in", "in"), .cols = ends_with("mm_in")) %>% 
+
+  island_data_means_imperial <- island_data_means_imperial %>%
+    rename_with(~stringr::str_replace(., "mm_in", "in"), .cols = ends_with("mm_in")) %>%
     rename_with(~stringr::str_replace(., "g_lb", "lb"), .cols = ends_with("g_lb"))
-  
+
   island_data_means_imperial <- island_data_means_imperial %>%
     select(where(is.character) |
              ends_with("in") |
              ends_with("lb"))
 
   return(island_data_means_imperial)
-  
+
 }
 
-my_function("https://github.com/cct-datascience/repro-data-sci/raw/r-lessons/lessons/7-intermediate-r-1/lesson-data/Biscoe.csv")
 
 my_function()
 
-function_output <-  my_function()
-torgersen_output <- my_function("https://github.com/cct-datascience/repro-data-sci/raw/r-lessons/lessons/7-intermediate-r-1/lesson-data/Torgersen.csv")
-dream_output <- my_function("https://github.com/cct-datascience/repro-data-sci/raw/r-lessons/lessons/7-intermediate-r-1/lesson-data/Dream.csv")
+biscoe_output <-  my_function(island_to_use = "Biscoe")
+torgersen_output <-  my_function(island_to_use = "Torgersen")
 
+
+biscoe_output <-  my_function(island_to_use = "lol")
+
+# For loops ---------------------------------------------------------------
+
+islands_we_want <- c("Biscoe", "Dream", "Torgersen")
+cleaned_data <- list()
+
+for(i in 1:3){
+  
+  # my_function(islands_we_want[1])
+  cleaned_data[[i]] <-my_function(islands_we_want[i])
+
+  }
+
+names(cleaned_data) <- island_we_want
+
+cleaned_data_df <- bind_rows(cleaned_data, .id = "island")
 
 
 
