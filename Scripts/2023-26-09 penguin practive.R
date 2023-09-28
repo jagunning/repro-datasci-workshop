44b98934e03b62fa873519124f35f4f58fc61096
Jordan Gunning <jordangunning@arizona.edu>
2023-09-28 11:45:03 -0700

    comment script

--- Scripts/2023-26-09 penguin practive.R
+++ Scripts/2023-26-09 penguin practive.R
@@ -1,48 +1,48 @@
-library(dplyr)
-
-biscoe_data <- read.csv("https://github.com/cct-datascience/repro-data-sci/raw/r-lessons/lessons/7-intermediate-r-1/lesson-data/Biscoe.csv")
-
-head(biscoe_data) 
-
-anyNA(biscoe_data)
-
-biscoe_data %>% na.omit()
-
-biscoe_data <- biscoe_data %>% na.omit()
-
-biscoe_data_means <- biscoe_data %>% group_by(species, sex) %>% 
-  summarize(mean_bill_length = mean(bill_length_mm))
-
-biscoe_data_means <- biscoe_data %>% group_by(species, sex) %>% 
-  summarize(across(ends_with("mm") |  ends_with("g"), mean))
-
-biscoe_data_means_imperial <- biscoe_data_means %>%
-  mutate(across(ends_with("mm"), ~. *0.03937008, .names = "{.col}_in"),
-         across(ends_with("g"), ~. * 0.002204623, .names = "{.col}_lb"))
-
-library(stringr)
-
-biscoe_data_means_imperial <- biscoe_data_means %>%
-  mutate(across(ends_with("mm"), ~. *0.03937008, .names = "{.col}_in"))
-
-biscoe_data_means_imperial <- biscoe_data_means_imperial %>% 
-  rename(flipper_length_in = flipper_length_mm_in)
-
-biscoe_data_means_imperial <- biscoe_data_means_imperial %>% 
-  rename_with(~stringr::str_replace(., "mm_in", "in"), .cols = ends_with("mm_in")) %>% 
-  rename_with(~stringr::str_replace(., "g_lb", "lb"), .cols = ends_with("g_lb"))
-
-biscoe_data_means_imperial <- biscoe_data_means_imperial %>% 
-  select(where(is.character) |
-         ends_with("in") |
-         ends_with("lb"))
-
-biscoe_data_means_imperial <- biscoe_data_means_imperial %>% 
-  select(c(where(is.character))) &
-  ends_with("in")
-
-# select pulls out columns
-# filter pulls out rows
+# library(dplyr)
+# 
+# biscoe_data <- read.csv("https://github.com/cct-datascience/repro-data-sci/raw/r-lessons/lessons/7-intermediate-r-1/lesson-data/Biscoe.csv")
+# 
+# head(biscoe_data) 
+# 
+# anyNA(biscoe_data)
+# 
+# biscoe_data %>% na.omit()
+# 
+# biscoe_data <- biscoe_data %>% na.omit()
+# 
+# # biscoe_data_means <- biscoe_data %>% group_by(species, sex) %>% 
+# #   summarize(mean_bill_length = mean(bill_length_mm))
+# 
+# biscoe_data_means <- biscoe_data %>% group_by(species, sex) %>% 
+#   summarize(across(ends_with("mm") |  ends_with("g"), mean))
+# 
+# biscoe_data_means_imperial <- biscoe_data_means %>%
+#   mutate(across(ends_with("mm"), ~. *0.03937008, .names = "{.col}_in"),
+#          across(ends_with("g"), ~. * 0.002204623, .names = "{.col}_lb"))
+# 
+# library(stringr)
+# 
+# biscoe_data_means_imperial <- biscoe_data_means %>%
+#   mutate(across(ends_with("mm"), ~. *0.03937008, .names = "{.col}_in"))
+# 
+# biscoe_data_means_imperial <- biscoe_data_means_imperial %>% 
+#   rename(flipper_length_in = flipper_length_mm_in)
+# 
+# biscoe_data_means_imperial <- biscoe_data_means_imperial %>% 
+#   rename_with(~stringr::str_replace(., "mm_in", "in"), .cols = ends_with("mm_in")) %>% 
+#   rename_with(~stringr::str_replace(., "g_lb", "lb"), .cols = ends_with("g_lb"))
+# 
+# biscoe_data_means_imperial <- biscoe_data_means_imperial %>% 
+#   select(where(is.character) |
+#          ends_with("in") |
+#          ends_with("lb"))
+# 
+# biscoe_data_means_imperial <- biscoe_data_means_imperial %>% 
+#   select(c(where(is.character))) &
+#   ends_with("in")
+# 
+# # select pulls out columns
+# # filter pulls out rows
   
 
 
@@ -66,8 +66,8 @@ my_function <- function(data_url = "https://github.com/cct-datascience/repro-dat
   island_data <- read.csv(data_url)
   island_data <- island_data %>% na.omit()
   
-  island_data_means <- island_data %>% group_by(species, sex) %>% 
-    summarize(mean_bill_length = mean(bill_length_mm))
+  # island_data_means <- island_data %>% group_by(species, sex) %>% 
+  #   summarize(mean_bill_length = mean(bill_length_mm))
   
   island_data_means <- island_data %>% group_by(species, sex) %>% 
     summarize(across(ends_with("mm") |  ends_with("g"), mean))
@@ -76,22 +76,20 @@ my_function <- function(data_url = "https://github.com/cct-datascience/repro-dat
     mutate(across(ends_with("mm"), ~. *0.03937008, .names = "{.col}_in"),
            across(ends_with("g"), ~. * 0.002204623, .names = "{.col}_lb"))
 
-  island_data_means_imperial <- biscoe_data_means %>%
-    mutate(across(ends_with("mm"), ~. *0.03937008, .names = "{.col}_in"))
+  # island_data_means_imperial <- biscoe_data_means %>%
+  #   mutate(across(ends_with("mm"), ~. *0.03937008, .names = "{.col}_in"))
   
-  island_data_means_imperial <- island_data_means_imperial %>% 
-    rename(flipper_length_in = flipper_length_mm_in)
+  # island_data_means_imperial <- island_data_means_imperial %>% 
+  #   rename(flipper_length_in = flipper_length_mm_in)
   
   island_data_means_imperial <- island_data_means_imperial %>% 
     rename_with(~stringr::str_replace(., "mm_in", "in"), .cols = ends_with("mm_in")) %>% 
     rename_with(~stringr::str_replace(., "g_lb", "lb"), .cols = ends_with("g_lb"))
   
-  island_data_means_imperial <- island_data_means_imperial %>% 
+  island_data_means_imperial <- island_data_means_imperial %>%
     select(where(is.character) |
              ends_with("in") |
              ends_with("lb"))
-
-  
 
   return(island_data_means_imperial)
   
@@ -102,7 +100,8 @@ my_function("https://github.com/cct-datascience/repro-data-sci/raw/r-lessons/les
 my_function()
 
 function_output <-  my_function()
-
+torgersen_output <- my_function("https://github.com/cct-datascience/repro-data-sci/raw/r-lessons/lessons/7-intermediate-r-1/lesson-data/Torgersen.csv")
+dream_output <- my_function("https://github.com/cct-datascience/repro-data-sci/raw/r-lessons/lessons/7-intermediate-r-1/lesson-data/Dream.csv")
 
 
 
